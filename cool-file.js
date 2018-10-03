dc45c5c5a61e335f577c3e99e1c74e792c32a90b
Glitch (simple-js-quiz) <none>
2018-10-03 08:45:41 +0000

    quiz jsonをfor each中でDOM操作し描画（やや疑問点あり）

--- cool-file.js
+++ cool-file.js
@@ -1,4 +1,76 @@
+const myQuestions = [
+  {
+    question: "Who is the strongest?",
+    answers: {
+      a: "Superman",
+      b: "The Terminator",
+      c: "Waluigi, obviously"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "What is the best site ever created?",
+    answers: {
+      a: "SitePoint",
+      b: "Simple Steps Code",
+      c: "Trick question; they're both the best"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Where is Waldo really?",
+    answers: {
+      a: "Antarctica",
+      b: "Exploring the Pacific Ocean",
+      c: "Sitting in a tree",
+      d: "Minding his own business, so stop asking"
+    },
+    correctAnswer: "d"
+  }
+];
+
+const output = [];
+//outputをglobal化した
 function buildQuiz(){
+  //const output = [];
+  
+  myQuestions.forEach(
+  //forEachループなので、現在の値とインデックス（配列中の現在のアイテムの位置を示す数字）と配列をパラメーターとして受け取ります
+  //現在の値とインデックスが必要で、それぞれcurrentQuestionとquestionNumberと名付けています。
+    (currentQuestion, questionNumber)=> {
+      console.log(" currentQuestion: "+currentQuestion);
+      console.log(" currentQuestion: "+currentQuestion+ " questionNumber: "+questionNumber);
+      
+      const answers = [];
+      
+      //for(letter in currentQuestion.answers){だとエラー出た..
+      for(var letter in currentQuestion.answers){
+        console.log(" currentQuestion.answers: "+currentQuestion.answers + " letter: "+ letter);
+        //currentQuestion.answersはobjectしか見えない
+        
+        
+      //それぞれの質問に対してHTMLを生成します。まずは解答の選択肢を格納する配列を用意します。
+        answers.push(
+          //pushについて
+        `<label>
+         <input type="radio" name="question${questionNumber}" value="${letter}">
+         ${letter}:
+         ${currentQuestion.answers[letter]}
+        </label>`
+        );
+      } //end for
+      
+        // add this question and its answers to the output
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+           <div class="answers"> ${answers.join("")}</div>`
+        );
+        
+      
+      
+      
+    }
+  );
   console.log('buildQuiz');
 }
 
@@ -7,7 +79,7 @@ function showResults(){
 }
 
 // display quiz right away
-buildQuiz();
+//buildQuiz();
 
 
 
@@ -29,6 +101,10 @@ console.log(quizContainer);
 console.log(resultsContainer);
 console.log(resultsContainer);
   
+  //display quiz right away
+  buildQuiz();
+  //outputは別の関数なので呼べない。どうすれば良いか
+  quizContainer.innerHTML = output.join('');
   
   // on submit, show results
   //submitButtonのDOMはonload中でしか取れないのでここしかかけない？
